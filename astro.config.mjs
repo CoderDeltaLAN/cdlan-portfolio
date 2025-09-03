2acde72603089c6b8c354ca652e102d1c2320b02
CoderDeltaLan <coderdeltalan@gmail.com>
2025-09-03 04:17:45 +0100

    fix(pages): trailing slash in base for correct BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://coderdeltalan.github.io',
-  base: '/cdlan-portfolio',
+  base: '/cdlan-portfolio/',
   output: 'static',
   build: { inlineStylesheets: 'auto' },
 });
